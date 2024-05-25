f8ba10eb7b9db36abd47be5752d34cac8bbc15f2
Ignacio de la Arena R. R. <ignaciodelaarenarr@proton.me>
2024-05-25 09:52:48 -0300

    first poem

---
+++ a_raposa_e_as_uvas.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("A Raposa e as Uvas\n");
+        printf("\n");
+        printf("Contam que certa raposa,\n");
+        printf("Andando muito esfaimada,\n");
+        printf("Viu roxos maduros cachos\n");
+        printf("Pendentes de alta latada.\n");
+        printf("\n");
+        printf("De bom grado os trincaria,\n");
+        printf("Mas sem lhes poder chegar,\n");
+        printf("Disse: \"Estão verdes não prestam,\n");
+        printf("Só os cães os podem tragar!\"\n");
+        printf("\n");
+        printf("Eis cai uma parra, quando\n");
+        printf("Prosseguia seu caminho,\n");
+        printf("E, crendo que era algum bago,\n");
+        printf("Volta depressa o focinho.\n");
+}
